35d8b3025f4bd040c79d903891adcd54e6b40a89
Gabriel <gabrielmelo@ingressolive.com>
2025-02-19 14:26:25 -0300

    update

--- android/src/main/java/com/vicoaguado/flutter_wallet/FlutterWalletPlugin.java
+++ android/src/main/java/com/vicoaguado/flutter_wallet/FlutterWalletPlugin.java
@@ -1,42 +1,33 @@
 package com.vicoaguado.flutter_wallet;
 
 import androidx.annotation.NonNull;
-
-import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-
 
 /** FlutterWalletPlugin */
-public class FlutterWalletPlugin implements MethodCallHandler, FlutterPlugin {
-
-  MethodChannel channel;
-
-  @Override
-  public void onAttachedToEngine(FlutterPluginBinding binding) {
-    _onAttachedToEngine(binding.getBinaryMessenger());
-  }
+public class FlutterWalletPlugin implements FlutterPlugin, MethodCallHandler {
+  private MethodChannel channel;
 
   @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
-
-  private void _onAttachedToEngine(BinaryMessenger messenger) {
-    channel = new MethodChannel(messenger, "flutter_wallet");
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_wallet");
     channel.setMethodCallHandler(this);
   }
 
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else {
       result.notImplemented();
     }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
 }
